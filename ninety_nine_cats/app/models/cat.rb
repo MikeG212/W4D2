ae693d0a0d2d4c5dd426d01df9f848529416ad98
Michael <Gee>
2018-09-11 17:08:11 -0700

    Populate cat database and create restful route methods

--- ninety_nine_cats/app/models/cat.rb
+++ ninety_nine_cats/app/models/cat.rb
@@ -1,8 +1,10 @@
 class Cat < ApplicationRecord
-  validates :birth_date, :name, :color, :sex, :description, presence: true
-  validates :color, inclusion: { in: %w(red yellow black blue green tan white orange), message: "Not a valid color. Choose from red, yellow, black, blue, green, tan, white, orange."
-  validates :sex, inclusion: { in: %w(M F), message: "Not a valid sex. Choose from M or F."
+  CAT_COLORS = %w(red yellow black blue green tan white orange)
   
+  validates :birth_date, :name, :color, :sex, :description, presence: true
+  validates :color, inclusion: { in: CAT_COLORS}
+  validates :sex, inclusion: { in: %w(M F) }
+
   def age
    created_at.year - birth_date.year
   end
